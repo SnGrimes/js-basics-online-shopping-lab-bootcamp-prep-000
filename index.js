a3af5ec1f707bde9d7f5a8e2752cbf1cd651e2a3
Shantia Grimes-Farmer <shantiagrimes@gmail.com>
2018-06-13 15:48:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined) {
     return `Sorry, we don't have a credit card on file for you.`;
   }
-  let total = total();
+  const total = total();
   card = [];
   return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
 }
